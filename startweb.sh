9822c35f9d6ff2caad1c8dc99aa059c0d675b93a
poikilos <7557867+poikilos@users.noreply.github.com>
2020-03-15 09:39:35 -0400

    Respect the separation from EnlivenMinetest in the screen name.

--- startweb.sh
+++ startweb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -f "`command -v screen`" ]; then
-    screen -S EnlivenMinetest node server.js
+    screen -S enliven-node node server.js
 else
     nohup node server.js &
     echo "node server.js is running in background. terminate as follows:"
